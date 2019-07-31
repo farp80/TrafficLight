98b3e113898fb0f79358c61801b8bdab65040af3
farp80 <farp80@gmail.com>
2019-07-31 17:09:30 +0000

    glowing added

--- src/js/component/trafficLight.js
+++ src/js/component/trafficLight.js
@@ -9,13 +9,44 @@ export class TrafficLight extends React.Component {
 	}
 
 	render() {
+		let greenGlow = "";
+		let redGlow = "";
+		let yellowGlow = "";
+
+		switch (this.state.selectedColor) {
+			case "red":
+				redGlow = "glow";
+				break;
+
+			case "yellow":
+				yellowGlow = "glow";
+				break;
+
+			case "green":
+				greenGlow = "glow";
+				break;
+		}
+
 		return (
 			<div className="text-center mt-5">
 				<div className="thinnerDiv pt-1" />
 				<div className="widerDiv">
-					<div className="dot red mb-3" />
-					<div className="dot yellow mb-3" />
-					<div className="dot green" />
+					<div
+						className={"dot red mb-3 " + redGlow}
+						onClick={() => this.setState({ selectedColor: "red" })}
+					/>
+					<div
+						className={"dot yellow mb-3 " + yellowGlow}
+						onClick={() =>
+							this.setState({ selectedColor: "yellow" })
+						}
+					/>
+					<div
+						className={"dot green " + greenGlow}
+						onClick={() =>
+							this.setState({ selectedColor: "green" })
+						}
+					/>
 				</div>
 			</div>
 		);
